dfa2b95f6f024161d4b6c9542f2ed3951685a7b4
Kanishk Nagpal <kaynagpal03@gmail.com>
2024-03-01 23:41:09 +0530

    defined a file structure tree

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("Hello, World!");
+
+const { BrowserWindow, app } = require("electron")
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        title: "Document Explorer",
+        width: 500,
+        height: 600
+    });
+
+    mainWindow.loadFile()
+}
